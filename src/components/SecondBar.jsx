9dbb9d36a1467a7d4977f716c896e26291829a79
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2022-10-21 03:09:53 +0300

    react

---
+++ src/components/SecondBar.jsx
@@ -0,0 +1,25 @@
+import '../index.css'
+
+export default function SecondBar() {
+  return(
+    <div className='px-56 py-2 bg-white h-[40px] w-full flex p-5 gap-6 whitespace-nowrap '>
+      <p>Yazılım Geliştirme</p>
+      <p>İşletme</p>
+      <p>Finans ve Muhasebe</p>
+      <p>BT ve Yazılım</p>
+      <p>Ofiste Verimlilik</p>
+      <p>Kişisel Gelişim</p>
+      <p>Tasarım</p>
+      <p>Pazarlama</p>
+      <p>Sağlık ve Fitness</p>
+      <p>Müzik</p>
+
+      <img width="1340" height="400" alt="" src="https://img-c.udemycdn.com/notices/featured_banner/image_udlite/542775ce-985d-4103-8f86-1bfc28afb29d.jpg"></img>
+
+
+
+
+    </div>
+
+  )
+}
